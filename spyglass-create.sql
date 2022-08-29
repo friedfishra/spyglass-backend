5944db7b40c93e21a744f7161432afaad4b0081b
Atul Mishra <69535893+friedfishra@users.noreply.github.com>
2022-08-29 12:56:02 -0400

    Update spyglass-create.sql

--- spyglass-create.sql
+++ spyglass-create.sql
@@ -1,9 +1,10 @@
-START TRANSACTION;
 
-CREATE TABLE goals 
+DROP TABLE IF exists goal CASCADE;
+
+CREATE TABLE goal 
 (
 	id int not null auto_increment,
-    name varchar(50) not null,
+    name varchar(200) not null,
     description varchar(1024),
     image_path varchar(1024),
     target_date date not null,
